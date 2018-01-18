f3025ec7ea6ab84d49948d37edbe516b31d5c384
Mark Packham <markpackham1@gmail.com>
2018-01-18 22:52:58 +0000

    Creating functions to work with database in class

--- MySQLiWrapperSingletonPattern/classes/DB.php
+++ MySQLiWrapperSingletonPattern/classes/DB.php
@@ -2,5 +2,52 @@
 
 class DB
 {
+    private $_mysqli,
+        $_query,
+        $_results = array(),
+        $_count = 0;
 
+    public static $instance;
+
+    public static function getInstance()
+    {
+        if (!isset(self::$instance)) {
+            self::$instance = new DB();
+        }
+        return self::$instance;
+    }
+
+    //Connect to our database (only runs once)
+    public function __construct()
+    {
+        $this->_mysqli = new mysqli('localhost', 'root', '', 'singleton');
+
+        //Connection error check, and kill script
+        if ($this->_mysqli->connect_error) {
+            die($this->_mysqli->connect_error);
+        }
+
+    }
+
+    public function query($sql)
+    {
+        if ($this->_query = $this->_mysqli->query($sql)) {
+            while ($row = $this->_query->fetch_object()) {
+                $this->results[] = $row;
+            }
+            $this->_count = $this->_query->num_rows;
+        }
+        //Return the new database instance
+        return $this;
+    }
+
+    public function results()
+    {
+        return $this->_results;
+    }
+
+    public function count()
+    {
+        return $this->_count;
+    }
 }
--- MySQLiWrapperSingletonPattern/mysqliSingleton.php
+++ MySQLiWrapperSingletonPattern/mysqliSingleton.php
@@ -1,7 +1,8 @@
 <?php
 require_once('classes/DB.php');
 
-$users = DB::getInstance()->query('SELECT * FROM users');
+$users = DB::getInstance()->query('SELECT * FROM singleton');
+var_dump($users);
 if ($users->count()) {
     foreach ($users->results() as $user) {
         echo $user->name, '<br>';
