fe515581c2d7a2bcd2fb1b358aeb8fa605dfc262
Mark Packham <markpackham1@gmail.com>
2018-01-19 19:24:00 +0000

    Multiple File Uploading

--- UploadingFiles/upload.php
+++ UploadingFiles/upload.php
@@ -1,36 +1,61 @@
 <?php
-if (isset($_FILES['file'])) {
-    //The File mentioned in the form on uploadFile.html
-    $file = $_FILES['file'];
 
-    //File properties
-    $file_name = $file['name'];
-    //This is the location so we can later move the file
-    $file_tmp = $file['tmp_name'];
-    $file_size = $file['size'];
-    $file_error = $file['error'];
+if (!empty($_FILES['files']['name'][0])) {
+    $files = $_FILES['files'];
 
-    //White List our file extensions
-    $file_ext = explode('.', $file_name);
-    // end() always takes the end of an array (Set the internal pointer of an array to its last element)
-    $file_ext = strtolower(end($file_ext));
+    $uploaded = array();
+    $failed = array();
 
     $allowed = array('txt', 'jpg');
 
-    //in_array checks if a value exists in an array
-    if (in_array($file_ext, $allowed)) {
-        if ($file_error === 0) {
-            //Make sure file less the given size
-            if ($file_size <= 2000000) {
-                $file_name_new = uniqid('', true) . '.' . $file_ext;
-                //Chuck new files into our uploads directory
-                $file_destination = 'uploads/' . $file_name_new;
+    foreach ($files['name'] as $position => $file_name) {
 
-                if (move_uploaded_file($file_tmp, $file_destination)) {
-                    echo $file_destination;
+        //File Properties
+        $file_tmp = $files['tmp_name'][$position];
+        $file_size = $files['size'][$position];
+        $file_error = $files['error'][$position];
+        $file_ext = explode('.', $file_name);
+        $file_ext = strtolower(end($file_ext));
+
+        if (in_array($file_ext, $allowed)) {
+
+            //Check for file error code
+            if ($file_error === 0) {
+
+                //Check file size
+                if ($file_size <= 2000000) {
+
+                    //We want the file to have a unique name when moved
+                    $file_name_new = uniqid('', true) . '.' . $file_ext;
+                    //Where file uploaded to
+                    $file_destination = 'uploads/' . $file_name_new;
+
+                    if (move_uploaded_file($file_tmp, $file_destination)) {
+                        $uploaded[$position] = $file_destination;
+                    } else {
+                        $failed[$position] = "[{$file_name}] failed to upload";
+                    }
+
+                } else {
+                    $failed[$position] = "[{$file_name}] is too large";
                 }
 
+            } else {
+                $failed[$position] = "[{$file_name}] failed to upload, error code '{$file_error}'";
             }
+
+        } else {
+            $failed[$position] = "[{$file_name}] file extension '{$file_ext}' is not allowed";
         }
+    }
+
+    if (!empty($uploaded)) {
+        echo "Success, file upload details<br>";
+        print_r($uploaded);
+    }
+
+    if (!empty($failed)) {
+        echo "Failure details<br>";
+        print_r($failed);
     }
 }
