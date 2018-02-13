1c075b47af1f85d5c7fd61e759a9e4290cbe2f3f
Mark Packham <markpackham1@gmail.com>
2018-02-13 08:12:12 +0000

    unused css corrections

--- ReviseDerekBanas/learnphp.php
+++ ReviseDerekBanas/learnphp.php
@@ -8,7 +8,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
 <body>
 <header></header>
