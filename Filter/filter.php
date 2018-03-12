8e076a5dd2dade903f957bf57810aca58e637ef8
Mark Packham <markpackham1@gmail.com>
2018-03-12 19:52:17 +0000

    More filtering

--- Filter/filter.php
+++ Filter/filter.php
@@ -15,3 +15,13 @@ echo "Unsanitized email, $email";
 echo '<br />';
 $email = filter_var($email, FILTER_SANITIZE_EMAIL);
 echo "Sanitized email, $email";
+echo '<br /><br />';
+
+$evilString = "<h2><span style='background-color:red;'>I am a <em>bad</em> scriptØØØØØØ</span></h2>";
+echo $evilString;
+//Strip tags santizing the string
+$goodString = filter_var($evilString, FILTER_SANITIZE_STRING);
+echo $goodString.'<br />';
+//Get rid of ascii characters greater than 127
+$removeØ = filter_var($evilString, FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH);
+echo $removeØ.'<br />';
