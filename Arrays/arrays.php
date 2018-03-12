ce3cccfd47afa2f54eb88300cb90a2bb9de98a84
Mark Packham <markpackham1@gmail.com>
2018-03-12 21:14:56 +0000

    array_diff

---
+++ Arrays/arrays.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * Compares array1 against one or more other arrays and returns the values in array1 that are not present in any of the other arrays.
+ * http://php.net/manual/en/function.array-diff.php
+ */
+
+$array1=array("a"=>"1","b"=>"2","c"=>"3","d"=>"4");
+$array2=array("e"=>"1","f"=>"2","g"=>"30000");
+
+$output=array_diff($array1,$array2);
+print_r($output);
+//Shows the first result array's differences
+//Array ( [c] => 3 [d] => 4 )
