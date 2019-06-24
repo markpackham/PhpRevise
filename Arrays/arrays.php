751375f898adf399799cd0b773217b1b46de4828
Mark Packham <markpackham1@gmail.com>
2019-06-24 20:57:41 +0100

    array notes

--- Arrays/arrays.php
+++ Arrays/arrays.php
@@ -96,3 +96,80 @@ The array_shift() function removes the first element of an array.
 The array_unshift() function adds elements to the beginning of the array.
 We can use chained square brackets ([]) to access and change elements within a nested array.
  */
+
+//
+// Associative Array, changing & removing stuff
+$my_car = [
+    "oil" => "black and clumpy",
+    "brakes" => "new",
+    "tires" => "old with worn treads",
+    "filth" => "bird droppings",
+    "wiper fluid" => "full",
+    "headlights" => "bright",
+];
+print_r($my_car);
+$my_car["oil"] = "new and premium";
+$my_car["tires"] = "new with deep treads";
+unset($my_car["filth"]);
+print_r($my_car);
+
+//
+// Numerical keys in arrays example
+$hybrid_array = [1, 2, 3, 4];
+$hybrid_array[8] = "five more";
+print_r($hybrid_array);
+array_push($hybrid_array, rand());
+echo $hybrid_array[9];
+
+//
+//
+// Array by Value vs by Reference
+function changeColor($arr)
+{
+    $arr["color"] = "red";
+}
+$object = ["shape" => "square", "size" => "small", "color" => "green"];
+changeColor($object);
+echo $object["color"]; // Prints: green
+//
+function reallyChangeColor(&$arr)
+{
+    $arr["color"] = "red";
+}
+$object = ["shape" => "square", "size" => "small", "color" => "green"];
+reallyChangeColor($object);
+echo $object["color"]; // Prints: red
+///
+/// Another example of array by Value & array by Reference
+$doge_meme = ["top_text" => "Such Python", "bottom_text" => "Very language. Wow.", "img" => "very-cute-dog.jpg", "description" => "An adorable doge looks confused."];
+$bad_meme = ["top_text" => "i don't know", "bottom_text" => "i can't think of anything", "img" => "very-fat-cat.jpg", "description" => "A very fat cat looks happy."];
+$meme = ["top_text", "bottom_text", "img", "description"];
+function createMeme($meme)
+{
+    $meme["top_text"] = "Much PHP";
+    $meme["bottom_text"] = "Very programming. Wow.";
+    return $meme;
+}
+$php_doge = createMeme($doge_meme);
+print_r($php_doge);
+function fixMeme(&$meme)
+{
+    $meme["top_text"] = "Moo";
+    $meme["bottom_text"] = "Meow";
+    return $meme;
+}
+fixMeme($bad_meme);
+
+/*
+Associative arrays are data structures in which string or integer keys are associated with values.
+We use the => operator to associate a key with its value. $my_array = ["panda"=>"very cute"]
+To print an array’s keys and their values, we can use the print_r() function.
+We access the value associated with a given key by using square brackets ([ ]). For example: $my_array["panda"] will return "very cute".
+We can assign values to keys using this same indexing syntax and the assignment operator (=): $my_array["dog"] = "good cuteness";
+This same syntax can be used to change existing elements. $my_array["dog"] = "max cuteness";
+We can remove a key=>value pair entirely using the PHP unset() function.
+Keys can be integers. In fact, ordered arrays are just arrays in which integer keys have been assigned to the values automatically.
+In PHP, associative arrays and ordered arrays are different uses of the same data type.
+The union (+) operator takes two array operands and returns a new array with any unique keys from the second array appended to the first array.
+When writing function with array parameters, we can pass the array by value or by reference depending on our intent.
+ */
