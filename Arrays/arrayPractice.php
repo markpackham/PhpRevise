4d8e106b8ec4fa19c7c74d78c4b7da5c0361a4b6
Mark Packham <markpackham1@gmail.com>
2019-06-25 10:51:32 +0100

    Multi dimensional array practice

--- Arrays/arrayPractice.php
+++ Arrays/arrayPractice.php
@@ -29,5 +29,36 @@ Array ( [0] => 60 [1] => 61 [2] => Array ( [0] => 62 [1] => 99999 ) )
 $lv5 = $lv4[2];
 //print_r($lv5);
 // Array ( [0] => 62 [1] => 99999 ) // found it, last number has to be 1
-echo $multiDimensional[2][5][3][6][2][1];
+echo $multiDimensional[2][5][3][6][2][1] . "<br/>";
 // 99999
+
+///
+/// aim to echo out 998
+$multiDimensional2 = [1, [2, [3, [4, [5, [6, [7]]]]]], [21, [31, 998, [41, [51, [61, [71]]]]]]];
+//print_r($multiDimensional2);
+/*
+Array ( [0] => 1 [1] => Array ( [0] => 2 [1] => Array ( [0] => 3 [1] => Array ( [0] => 4 [1] => Array ( [0] => 5 [1] => Array
+( [0] => 6 [1] => Array ( [0] => 7 ) ) ) ) ) ) [2] => Array
+( [0] => 21 [1] => Array ( [0] => 31 [1] => 998 [2] => Array ( [0] => 41 [1] => Array ( [0] => 51 [1] => Array ( [0] => 61 [1] => Array ( [0] => 71 ) ) ) ) ) ) )
+ */
+$lv1 = $multiDimensional2[2];
+//print_r($lv1);
+// Array ( [0] => 21 [1] => Array ( [0] => 31 [1] => 998 [2] => Array ( [0] => 41 [1] => Array ( [0] => 51 [1] => Array ( [0] => 61 [1] => Array ( [0] => 71 ) ) ) ) ) )
+$lv2 = $lv1[1];
+//print_r($lv2);
+// Array ( [0] => 31 [1] => 998 [2] => Array ( [0] => 41 [1] => Array ( [0] => 51 [1] => Array ( [0] => 61 [1] => Array ( [0] => 71 ) ) ) ) )
+echo $multiDimensional2[2][1][1] . "<br>";
+// 998
+
+//
+//
+// aim to echo out 997
+$multiDimensional3 = [[1], 11, [2], 22, [3], 33, [4], 44, [441, 442, 443, [997]], [5]];
+//print_r($multiDimensional3);
+/* Array ( [0] => Array ( [0] => 1 ) [1] => 11 [2] => Array ( [0] => 2 ) [3] => 22 [4] => Array ( [0] => 3 ) [5] => 33
+[6] => Array ( [0] => 4 ) [7] => 44 [8] => Array ( [0] => 441 [1] => 442 [2] => 443 [3] => Array ( [0] => 997 ) ) [9] => Array ( [0] => 5 ) )
+ */
+$lv1 = $multiDimensional3[8];
+//print_r($lv1);
+// Array ( [0] => 441 [1] => 442 [2] => 443 [3] => Array ( [0] => 997 ) )
+echo $multiDimensional3[8][3][0] . "<br>";
